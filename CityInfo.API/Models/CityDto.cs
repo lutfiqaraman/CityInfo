b3af72046bcfa0735e34123137b99291c02dfc89
lutfiqaraman <lutfiqaraman@gmail.com>
2022-08-21 21:12:35 +0300

    build pointofinterestdto

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -5,5 +5,15 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+        public int NumberOFPointsOfInterest 
+        {
+            get
+            {
+                return PointOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointOfInterest { get; set; } 
+            = new List<PointOfInterestDto>();
     }
 }
