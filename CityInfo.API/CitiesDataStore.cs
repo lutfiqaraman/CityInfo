29848c74ce967f858449c1176b14a87a9cda18c9
lutfiqaraman <lutfiqaraman@gmail.com>
2022-08-21 21:45:26 +0300

    building dto of the cities

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using System.Collections.Generic;
 
 namespace CityInfo.API
 {
@@ -15,21 +16,69 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "United Nations Headquarters",
+                            Description = "The building of United Nations"
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "The Museum of Modern Art",
+                            Description = "The most important museum in New York related to the modern art"
+                        }
+                    }
                 },
 
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Dubai",
-                    Description = "The one with that tallest tower in the world."
+                    Description = "The one with that tallest tower in the world.",
+                    PointOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Burj Khalifa",
+                            Description = "The world's tallest tower"
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Burj Al Arab",
+                            Description = "The most luxury hotel in the world"
+                        }
+                    }
                 },
 
                 new CityDto()
                 {
-                    Id = 1,
+                    Id = 3,
                     Name = "London",
-                    Description = "The one with that big dom."
+                    Description = "The one with that big dom.",
+                    PointOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Buckingham Palace",
+                            Description = "The biggest palace in the world"
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "British Museum",
+                            Description = "the biggest museum in London city"
+                        }
+                    }
                 },
 
 
