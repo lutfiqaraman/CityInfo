ba011d9fa973b8b2da89aee1c9bf3affb0009b3b
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-04 21:59:08 +0300

    create city entity

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,13 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+            = new List<PointOfInterestDto>();
+    }
+}
