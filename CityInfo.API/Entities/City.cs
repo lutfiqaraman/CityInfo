a0092337f0d9e7873578585029a3ee911f8f727c
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-05 17:32:09 +0300

    build city entity

--- CityInfo.API/Entities/City.cs
+++ CityInfo.API/Entities/City.cs
@@ -1,13 +1,22 @@
 ﻿using CityInfo.API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CityInfo.API.Entities
 {
     public class City
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
             = new List<PointOfInterestDto>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
     }
 }
