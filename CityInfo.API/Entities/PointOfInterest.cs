71e4e7537b2cb25dc51045a9ec4be6e83def25b0
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-05 17:35:38 +0300

    build point of interest entity

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
