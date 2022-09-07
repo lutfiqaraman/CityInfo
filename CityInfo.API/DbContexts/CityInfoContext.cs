01b84c7311f4850c6e010b2657115244a8c3bcab
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-07 20:58:28 +0300

    build the dbcontext

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityInfo.API.DbContexts
 {
     public class CityInfoContext : DbContext
     {
-
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
     }
 }
