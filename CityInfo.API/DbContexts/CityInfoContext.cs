31b2e602015ad373fb21196692a25ccf543406d1
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-10 10:31:51 +0300

    Seeding data

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -5,13 +5,78 @@ namespace CityInfo.API.DbContexts
 {
     public class CityInfoContext : DbContext
     {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
+
         public CityInfoContext(DbContextOptions options)
             : base(options)
         {
 
         }
 
-        public DbSet<City> Cities { get; set; } 
-        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>()
+                .HasData(
+
+                new City("New York City")
+                {
+                    Id = 1,
+                    Description = "The one with that big park."
+                },
+
+                new City("Dubai")
+                {
+                    Id = 2,
+                    Description = "The one with that tallest tower in the world."
+                },
+
+                new City("London")
+                {
+                    Id = 3,
+                    Description = "The one with that big dom."
+                });
+
+            modelBuilder.Entity<PointOfInterest>()
+                .HasData(
+
+                new PointOfInterest("United Nations Headquarters", "The building of United Nations")
+                {
+                    Id= 1,
+                    CityId = 1                    
+                },
+
+                new PointOfInterest("The Museum of Modern Art", "The most important museum in New York related to the modern art")
+                {
+                    Id = 2,
+                    CityId = 1
+                },
+
+                new PointOfInterest("Burj Khalifa", "The tallest tower in the world")
+                {
+                    Id = 3,
+                    CityId = 2
+                },
+
+                new PointOfInterest("Burj Al Arab", "The most luxury hotel in the world")
+                {
+                    Id = 4,
+                    CityId = 2
+                },
+
+                new PointOfInterest("Buckingham Palace", "The biggest palace in the world")
+                {
+                    Id = 5,
+                    CityId = 3
+                },
+
+                new PointOfInterest("British Museum", "The biggest museum in London city")
+                {
+                    Id = 6,
+                    CityId = 3
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CityInfo.API/Migrations/20220910072829_DataSeed.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "The one with that big park.", "New York City" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "The one with that tallest tower in the world.", "Dubai" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "The one with that big dom.", "London" });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The building of United Nations", "United Nations Headquarters" },
+                    { 2, 1, "The most important museum in New York related to the modern art", "The Museum of Modern Art" },
+                    { 3, 2, "The tallest tower in the world", "Burj Khalifa" },
+                    { 4, 2, "The most luxury hotel in the world", "Burj Al Arab" },
+                    { 5, 3, "The biggest palace in the world", "Buckingham Palace" },
+                    { 6, 3, "The biggest museum in London city", "British Museum" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
