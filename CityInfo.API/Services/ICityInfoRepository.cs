c82eec2d03177231a82a76431109943e96b3bb68
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-13 23:05:50 +0300

    build the repository

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -5,6 +5,7 @@ namespace CityInfo.API.Services
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>> GetCities();
-        Task<City?> GetCityById(int cityId); 
+        Task<City?> GetCityById(int cityId);
+        Task <IEnumerable<PointOfInterest>> GetPointsOfInterestForCity(int cityId);
     }
 }
